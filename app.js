a6c764e118ab4abf45ecb20d992463edefd1aace
Andres Carrizosa <andlesh_halo@hotmail.com>
2026-08-23 23:06:20 -0600

    Se ha agregado el archivo js con variables que se imprimen con un alert :D

---
+++ app.js
@@ -0,0 +1,18 @@
+let nombre1 = "Rise of the Tomb Raider"
+let anio1 = "2015"
+let favorito1 = true
+
+let nombre2 = "Halo"
+let anio2 = "2001"
+let favorito2 = true
+
+let nombre3 = "The Evil Within"
+let anio3 = "2014"
+let favorito3 = true
+
+console.log("Mi primer videojuego favorito es " + nombre1 + " con fecha de lanzamiento " + anio1 + " confirmo con " + favorito1 + " como mi favorito" );
+alert("Mi primer videojuego favorito es " + nombre1 + " con fecha de lanzamiento " + anio1 + " confirmo con " + favorito1 + " como mi favorito" );
+console.log("Mi primer videojuego favorito es " + nombre2 + " con fecha de lanzamiento " + anio2 + " confirmo con " + favorito2 + " como mi favorito" );
+alert("Mi primer videojuego favorito es " + nombre2 + " con fecha de lanzamiento " + anio2 + " confirmo con " + favorito2 + " como mi favorito" );
+console.log("Mi primer videojuego favorito es " + nombre3 + " con fecha de lanzamiento " + anio3 + " confirmo con " + favorito3 + " como mi favorito" );
+alert("Mi primer videojuego favorito es " + nombre3 + " con fecha de lanzamiento " + anio3 + " confirmo con " + favorito3 + " como mi favorito" );
